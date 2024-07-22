ce8394aa2dbfce57d151a59e7d03f1b77dc81e87
ITBBR16 <daniel.itrumahdrone@gmail.com>
2024-07-22 12:05:42 +0700

    Try api payment kios

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/api/updatePayment', [KiosPaymentController::class, 'updatePayment']);
+Route::post('/updatePayment', [KiosPaymentController::class, 'updatePayment']);
