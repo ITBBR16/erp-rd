1f845f9a447cec77f7ee12b08c11c4501baee378
ITBBR16 <daniel.itrumahdrone@gmail.com>
2025-01-07 11:24:37 +0700

    Fixed Customer Sementara

--- app/Services/repair/CustomerService.php
+++ app/Services/repair/CustomerService.php
@@ -32,9 +32,8 @@ class CustomerService
                 'no_telpon' => preg_replace('/^0/', '62', $request->input('no_telpon')),
             ]);
 
-            $validate = $request->validate([
+            $request->validate([
                 'first_name' => 'required|max:50',
-                'last_name' => 'required|max:50',
                 'asal_informasi' => 'required',
                 'no_telpon' => ['required', Rule::unique('rumahdrone_customer.customer', 'no_telpon')],
                 'email' => 'nullable|email:dns',
@@ -60,25 +59,25 @@ class CustomerService
             ];
 
             $dataCustomer = $this->customerRepository->createCustomer($dataInput);
+            $this->customerRepository->commitTransaction();
+            return ['status' => 'success', 'message' => 'Berhasil membuat data customer baru.'];
 
-            $appScriptUrl = 'https://script.google.com/macros/s/AKfycbyFTLvq0HaGhnZBjSWH3JLKuRntth2wBKoltkFrGwWQM0UHjG6BMLeaM3guaz9mLCS8/exec';
-            $response = Http::post($appScriptUrl, [
-                'first_name' => $validate['first_name'],
-                'last_name' => $validate['last_name'] . ' - ' . $dataCustomer->id,
-                'email' => $validate['email'],
-                'no_telpon' => $validate['no_telpon'],
-            ]);
+            // $appScriptUrl = 'https://script.google.com/macros/s/AKfycbyFTLvq0HaGhnZBjSWH3JLKuRntth2wBKoltkFrGwWQM0UHjG6BMLeaM3guaz9mLCS8/exec';
+            // $response = Http::post($appScriptUrl, [
+            //     'first_name' => $request->input('first_name'),
+            //     'last_name' => $request->input('last_name') . ' - ' . $dataCustomer->id,
+            //     'email' => $request->input('email'),
+            //     'no_telpon' => $request->input('no_telpon'),
+            // ]);
 
-            $payloadContact = json_decode($response->body(), true);
-            $statusContact = $payloadContact['status'];
-
-            if ($statusContact === 'success') {
-                $this->customerRepository->commitTransaction();
-                return ['status' => 'success', 'message' => 'Berhasil membuat data customer baru.'];
-            } else {
-                $this->customerRepository->rollbackTransaction();
-                return ['status' => 'error', 'message' => 'Gagal menyimpan kontak. Silahkan coba lagi'];
-            }
+            // $payloadContact = json_decode($response->body(), true);
+            // $statusContact = $payloadContact['status'];
+            
+            // if ($statusContact === 'success') {
+            // } else {
+            //     $this->customerRepository->rollbackTransaction();
+            //     return ['status' => 'error', 'message' => 'Gagal menyimpan kontak. Silahkan coba lagi'];
+            // }
 
         } catch (Exception $e) {
             $this->customerRepository->rollbackTransaction();
