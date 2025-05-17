a6d32b6ddc9ac16c3faccb04b40f4fe9763ff122
ITBBR16 <daniel.itrumahdrone@gmail.com>
2025-05-17 22:32:23 +0700

    Change controller certificate

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use App\Mail\CertificateMail;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class CertificateController extends Controller
 {
@@ -22,18 +23,31 @@ class CertificateController extends Controller
     public function sendSertificate(Request $request)
     {
         $request->validate([
-            'nama' => 'required',
-            'email' => 'required'
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'sender' => 'required|email',
+            'body' => 'required|string',
         ]);
 
-        $name = $request->nama;
-        $email = $request->email;
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $senderEmail = $request->input('sender');
+        $bodyEmail = $request->input('body');
 
-        $pdf = PDF::loadView('certificate.certificate-template', compact('name'));
-        $pdfData = $pdf->output();
+        $pdf = Pdf::loadView('certificate.template', compact('name'))->setPaper('a4', 'landscape');
+        $pdfContent = $pdf->output();
 
-        Mail::to($email)->send(new CertificateMail($name, $pdfData));
+        Mail::send([], [], function ($message) use ($email, $name, $senderEmail, $bodyEmail, $pdfContent) {
+            $message->from($senderEmail, 'Certificate Sender')
+                    ->to($email)
+                    ->subject("Certificate for $name")
+                    ->attachData($pdfContent, "Certificate - $name.pdf", [
+                        'mime' => 'application/pdf',
+                    ])
+                    ->setBody($bodyEmail, 'text/html');
+        });
 
+        Log::info("Certificate sent to $name <$email> by $senderEmail");
         return response()->json(['message' => 'Sertifikat terkirim ke ' . $email], 200);
     }
 }
